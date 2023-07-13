5e0254a21bf8b673f647c3bc3683834ba4402d43
Alexandra Ruth Fogg <arfogg@cp.dias.ie>
2023-07-13 16:46:51 +0100

    changing top panels to histograms

--- determine_AD_AI.py
+++ determine_AD_AI.py
@@ -12,6 +12,7 @@ import scipy
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib.gridspec import GridSpec
 
 # R code by Daire
 # set.seed(12)
@@ -85,3 +86,85 @@ def test_check_AD_AI():
         
     fig, ax=plt.subplots()
     ax.plot(u,chi)
+    
+    
+    
+    
+def plot_extremal_dependence_coefficient(x_data,y_data, x_name, y_name, x_units, y_units, csize=17):
+    
+    # # TEST DATA
+    # # Making the same function as in Daire's code
+    # mean = [0, 0]
+    # cov = [[1, 0.95], [0.95, 1]]
+    # x_data, y_data = np.random.multivariate_normal(mean, cov, 10000).T
+    
+    
+    
+    # Define plotting window for generic test plotting
+    fig=plt.figure(layout="constrained",figsize=(15,11))
+    gs=GridSpec(2,2,figure=fig)
+
+    # Define three axes to plot the data on
+    ax_data=fig.add_subplot(gs[0,0])
+    ax_data_unif=fig.add_subplot(gs[0,1])
+    ax_edc=fig.add_subplot(gs[1,:])
+    
+    # Plot the original input data
+    #ax_data.plot(x_data,y_data,linewidth=0.0,marker="o", fillstyle='none', color='grey')
+    h_data=ax_data.hist2d(x_data, y_data, bins=25, density=True)
+    cb_data=fig.colorbar(h_data[3],ax=ax_data)
+    # Formatting
+    ax_data.set_xlabel(x_name+' '+x_units, fontsize=csize)
+    ax_data.set_ylabel(y_name+' '+y_units, fontsize=csize)
+    for label in (ax_data.get_xticklabels() + ax_data.get_yticklabels()):
+        label.set_fontsize(csize)
+    cb_data.ax.tick_params(labelsize=csize)
+    cb_data.set_label("Normalised occurrence", fontsize=csize)
+    # Transform the variables into "uniform" - ask Daire
+    x_unif=scipy.stats.rankdata(x_data)/(x_data.size+1)
+    y_unif=scipy.stats.rankdata(y_data)/(y_data.size+1)
+    
+    # Plot out these uniform data
+    #ax_data_unif.plot(x_unif, y_unif, linewidth=0.0, marker='x', color='orange')
+    h_unif=ax_data_unif.hist2d(x_unif, y_unif, bins=25, density=True)
+    cb_unif=fig.colorbar(h_unif[3],ax=ax_data_unif)
+    # Formatting
+    ax_data_unif.set_xlabel(x_name+" normalised ????", fontsize=csize)
+    ax_data_unif.set_ylabel(y_name+" normalised ????", fontsize=csize)
+    for label in (ax_data_unif.get_xticklabels() + ax_data_unif.get_yticklabels()):
+        label.set_fontsize(csize)
+    cb_unif.ax.tick_params(labelsize=csize)
+    cb_unif.set_label("Normalised occurrence", fontsize=csize)
+    
+    # Calculate the "extremal dependence coefficient", called chi
+    #    for a range of quantiles u (from 0 to 1)
+    # if chi -> 0 as u -> 1, then x and y are asymptotically independent
+    #   otherwise they are asymtotically dependent
+    u=np.linspace(0,0.99,100)  # bins of width 0.01
+    chi=[]
+    for i in range(u.size):
+        top,=np.where((x_unif>u[i]) & (y_unif>u[i]))
+        bottom,=np.where(x_unif>u[i])
+        chi.append( (top.size)/(bottom.size) )
+
+    ax_edc.plot(u,chi, color='orange')
+    # Formatting
+    ax_edc.set_xlabel("Quantiles ????", fontsize=csize)
+    ax_edc.set_ylabel("Extremal Dependence Coefficient, $\chi$", fontsize=csize)
+    for label in (ax_edc.get_xticklabels() + ax_edc.get_yticklabels()):
+        label.set_fontsize(csize)
+    
+def test_plot_edc():
+    
+    mean = [0, 0]
+    cov = [[1, 0.95], [0.95, 1]]
+    x_data, y_data = np.random.multivariate_normal(mean, cov, 10000).T
+    
+    x_name='X'
+    y_name='Y'
+    x_units='(units)'
+    y_units='(units)'
+    
+    plot_extremal_dependence_coefficient(x_data, y_data, x_name, y_name, x_units, y_units)
+    
+    
