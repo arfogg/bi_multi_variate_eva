6e6405eaf63fd1a232e578d2629bcf4bfa8c41f5
Alexandra Ruth Fogg <arfogg@cp.dias.ie>
2023-09-25 17:35:47 +0100

    updating calculate_return_periods_values.plot_return_period_as_function_x_y

--- calculate_return_periods_values.py
+++ calculate_return_periods_values.py
@@ -9,6 +9,7 @@ Created on Fri Jul 21 14:40:39 2023
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 import transform_uniform_margins
 
@@ -48,6 +49,7 @@ def calculate_return_period(copula, sample, block_size=pd.to_timedelta("365.2425
 def plot_return_period_as_function_x_y(copula,min_x,max_x,min_y,max_y,x_name,y_name,n_samples=1000,
                                        block_size=pd.to_timedelta("365.2425D")):
     
+    # !!! should not be transformed empirically!!!
     # Create a sample
     sample=pd.DataFrame({x_name:transform_uniform_margins.transform_from_data_scale_to_uniform_margins_empirically(np.linspace(min_x,max_x,n_samples)),
                          y_name:transform_uniform_margins.transform_from_data_scale_to_uniform_margins_empirically(np.linspace(min_y,max_y,n_samples))})
@@ -55,14 +57,46 @@ def plot_return_period_as_function_x_y(copula,min_x,max_x,min_y,max_y,x_name,y_n
     # Create sample grid
     xv, yv = np.meshgrid(sample[x_name], sample[y_name])
     
-    sample_grid=pd.DataFrame({x_name:xv.ravel(), y_name:yv.ravel()})
-    # Calculate the return period
+    # sample_grid=pd.DataFrame({x_name:xv.ravel(), y_name:yv.ravel()})
+    # # Calculate the return period
+    # return_period=calculate_return_period(copula, sample_grid, block_size=block_size)
+
+    # # Initialise plotting window
+    # fig,ax=plt.subplots()
+    
+    # # Plot return period as a function of x and y
+    # ax.scatter(sample_grid[x_name], sample_grid[y_name], c=return_period, norm='linear')
+    
+    # plt.show()
+    
+    
+    mid_point_x=(xv[1:,1:]+xv[:-1,:-1])/2
+    mid_point_y=(yv[1:,1:]+yv[:-1,:-1])/2
+
+    raveled_mid_point_x=mid_point_x.ravel()
+    raveled_mid_point_y=mid_point_y.ravel()
+
+    sample_grid=pd.DataFrame({x_name:raveled_mid_point_x, y_name:raveled_mid_point_y})
     return_period=calculate_return_period(copula, sample_grid, block_size=block_size)
 
-    # Initialise plotting window
+    shaped_return_period=return_period.reshape(mid_point_x.shape)
+
+
+
     fig,ax=plt.subplots()
+
+    print(xv.shape, yv.shape, shaped_return_period.shape)
+    pcm=ax.pcolormesh(xv,yv,shaped_return_period, cmap='plasma', norm=colors.LogNorm(vmin=shaped_return_period.min(),
+                  vmax=shaped_return_period.max()*0.60))
     
-    # Plot return period as a function of x and y
-    ax.scatter(sample_grid[x_name], sample_grid[y_name], c=return_period, norm='linear')
+    fig.colorbar(pcm, ax=ax, extend='max', label='Return Period (units??)')
     
-    plt.show()
+    # Some Decor
+    ax.set_xlabel(x_name)
+    ax.set_ylabel(y_name)
+    
+    
+    
+    
+    
+    
