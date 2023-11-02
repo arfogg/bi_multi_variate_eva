6e336e1e236fdc74ad8df3779444d59d9bde4786
Alexandra Ruth Fogg <arfogg@cp.dias.ie>
2023-11-02 14:05:30 +0000

    trying different ways of plotting bivariate return period

--- calculate_return_periods_values.py
+++ calculate_return_periods_values.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import seaborn as sns
 
 from matplotlib.transforms import TransformedBbox
 from matplotlib.transforms import Bbox
@@ -156,7 +157,11 @@ def plot_return_period_as_function_x_y_3d(copula,min_x,max_x,min_y,max_y,x_name,
     surf = ax.plot_surface(mid_point_x_ds,mid_point_y_ds,shaped_return_period, cmap='plasma',
                            linewidth=0, antialiased=False)
     
+    print(np.min(shaped_return_period),np.max(shaped_return_period))
+    
     ax.set_zscale('log')
+    
+    
     # Customize the z axis.
     #ax.set_zlim(-1.01, 1.01)
     #ax.zaxis.set_major_locator(LinearLocator(10))
@@ -167,6 +172,13 @@ def plot_return_period_as_function_x_y_3d(copula,min_x,max_x,min_y,max_y,x_name,
     fig.colorbar(surf, shrink=0.5, aspect=5)
     
     plt.show()
+    
+    
+    # CUT OUT THE CORNER OF HIGH VALUES AND HAVE A LOOK AT THE
+    #   VARIABILITY ELSEWHERE
+    # POTENTIALLY VISUALISE LIKE THE WINE ATTRIBUTE HEATMAP HERE:
+    # https://towardsdatascience.com/the-art-of-effective-visualization-of-multi-dimensional-data-6c7202990c57
+    
     
     # # Plot return period as function of x and y in data scale
     # #pcm=ax.pcolormesh(xv_ds,yv_ds,shaped_return_period, cmap='plasma', norm=colors.LogNorm(vmin=shaped_return_period.min(),
@@ -180,7 +192,7 @@ def plot_return_period_as_function_x_y_3d(copula,min_x,max_x,min_y,max_y,x_name,
     #     c.set_clip_on(True)
     # clabels=ax.clabel(contours, inline=True, fmt="%0.1f")
 
-    # # Work out x and y limits based on parsed contour index
+    # Work out x and y limits based on parsed contour index
     # lower_ax_limit_contour_index=1
     # xy_contour_limit=contours.allsegs[lower_ax_limit_contour_index][0]
     # xlim=np.min(xy_contour_limit[:,0])*0.9
@@ -197,3 +209,202 @@ def plot_return_period_as_function_x_y_3d(copula,min_x,max_x,min_y,max_y,x_name,
     # ax.set_ylabel(y_label)
      
     return fig, ax
+
+
+def plot_return_period_as_function_x_y_experimenting(copula,min_x,max_x,min_y,max_y,x_name,y_name,x_gevd_fit_params, y_gevd_fit_params,
+                                       x_label, y_label, n_samples=1000,block_size=pd.to_timedelta("365.2425D"),
+                                       contour_levels=[1/12,0.5,1.0,10.0], lower_ax_limit_contour_index=1):
+    
+    # Set fontsize
+    csize=15
+    
+    # Create a sample
+    sample_um=pd.DataFrame({x_name:transform_uniform_margins.transform_from_data_scale_to_uniform_margins_empirically(np.linspace(min_x,max_x,n_samples)),
+                         y_name:transform_uniform_margins.transform_from_data_scale_to_uniform_margins_empirically(np.linspace(min_y,max_y,n_samples))})
+    sample_ds=pd.DataFrame({x_name:np.linspace(min_x,max_x,n_samples),
+                                    y_name:np.linspace(min_y,max_y,n_samples)})
+
+    # Create sample grid
+    xv_um, yv_um = np.meshgrid(sample_um[x_name], sample_um[y_name])    #uniform margins
+    xv_ds, yv_ds = np.meshgrid(sample_ds[x_name], sample_ds[y_name])    #data scale
+    # mesh grid on uniform margins for calculating, in data scale
+    #   for plotting
+      
+    # Determine mid point of each pixel to calculate return
+    #   period for
+    mid_point_x_um=(xv_um[1:,1:]+xv_um[:-1,:-1])/2
+    mid_point_y_um=(yv_um[1:,1:]+yv_um[:-1,:-1])/2
+    mid_point_x_ds=(xv_ds[1:,1:]+xv_ds[:-1,:-1])/2
+    mid_point_y_ds=(yv_ds[1:,1:]+yv_ds[:-1,:-1])/2
+
+    # Reshape
+    raveled_mid_point_x=mid_point_x_um.ravel()
+    raveled_mid_point_y=mid_point_y_um.ravel()
+    sample_grid=np.array([raveled_mid_point_x,raveled_mid_point_y]).T
+    
+    # Calculate return period
+    return_period=calculate_return_period(copula, sample_grid, block_size=block_size)
+    # Reshape for mesh grid
+    shaped_return_period=return_period.reshape(mid_point_x_um.shape)
+
+
+    # (1) 2D COLOUR PLOT WITH CONTOURS
+    # Initialise plot
+    fig, ax=plt.subplots(ncols=3, figsize=(27,7))
+
+    # NO FIDDLING, AS IT IS
+    # Plot return period as function of x and y in data scale
+    pcm=ax[0].pcolormesh(xv_ds,yv_ds,shaped_return_period, cmap='plasma', norm=colors.LogNorm())
+
+    # Contours
+    contours=ax[0].contour(mid_point_x_ds,mid_point_y_ds,shaped_return_period, contour_levels, colors='white')
+    for c in contours.collections:
+        c.set_clip_on(True)
+    clabels=ax[0].clabel(contours, inline=True, fmt="%0.1f")
+    
+    # Colourbar
+    cbar=fig.colorbar(pcm, ax=ax[0], extend='max', label='Return Period (years)')
+    cbar.add_lines(contours)  
+    
+    # Some Decor
+    ax[0].set_xlabel(x_label)
+    ax[0].set_ylabel(y_label)
+    
+    # Title
+    ax[0].set_title('No axes limit, unsaturated cbar', fontsize=csize)
+
+
+    # LIMIT AXES, SATURATE CBAR
+    # Plot return period as function of x and y in data scale
+    pcm=ax[1].pcolormesh(xv_ds,yv_ds,shaped_return_period, cmap='plasma', norm=colors.LogNorm(vmin=np.quantile(shaped_return_period, 0.1),
+                  vmax=np.quantile(shaped_return_period, 0.999)))
+
+    # Contours
+    contours=ax[1].contour(mid_point_x_ds,mid_point_y_ds,shaped_return_period, contour_levels, colors='white')
+    for c in contours.collections:
+        c.set_clip_on(True)
+    clabels=ax[1].clabel(contours, inline=True, fmt="%0.1f")
+
+    # Work out x and y limits based on parsed contour index
+    lower_ax_limit_contour_index=1
+    xy_contour_limit=contours.allsegs[lower_ax_limit_contour_index][0]
+    xlim=np.min(xy_contour_limit[:,0])*0.9
+    ylim=np.min(xy_contour_limit[:,1])*0.9
+    ax[1].set_xlim(left=xlim)
+    ax[1].set_ylim(bottom=ylim)
+    
+    # Colourbar
+    cbar=fig.colorbar(pcm, ax=ax[1], extend='max', label='Return Period (years)')
+    cbar.add_lines(contours)  
+    
+    # Some Decor
+    ax[1].set_xlabel(x_label)
+    ax[1].set_ylabel(y_label)
+    
+    # Title
+    ax[1].set_title('Limited axes limits (higher data shown),\nsaturated cbar 0.1-0.999 quantile', fontsize=csize)
+
+    
+    # GET RID OF VERY HIGH BIT
+    # Determine corner to remove
+    
+    # Plot return period as function of x and y in data scale
+    pcm=ax[2].pcolormesh(xv_ds,yv_ds,shaped_return_period, cmap='plasma', norm=colors.LogNorm(vmin=np.quantile(shaped_return_period, 0.1),
+                  vmax=np.quantile(shaped_return_period, 0.99)))
+
+    # Contours
+    contours=ax[2].contour(mid_point_x_ds,mid_point_y_ds,shaped_return_period, contour_levels, colors='white')
+    for c in contours.collections:
+        c.set_clip_on(True)
+    clabels=ax[2].clabel(contours, inline=True, fmt="%0.1f")
+
+    # Work out x and y limits based on parsed contour index
+    lower_ax_limit_contour_index=1
+    xy_contour_limit=contours.allsegs[lower_ax_limit_contour_index][0]
+    xlim=np.max(xy_contour_limit[:,0])*0.9
+    ylim=np.max(xy_contour_limit[:,1])*0.9
+    ax[2].set_xlim(right=xlim)
+    ax[2].set_ylim(top=ylim)
+    
+    # Colourbar
+    cbar=fig.colorbar(pcm, ax=ax[2], extend='max', label='Return Period (years)')
+    cbar.add_lines(contours)  
+    
+    # Some Decor
+    ax[2].set_xlabel(x_label)
+    ax[2].set_ylabel(y_label)
+    
+    # Title
+    ax[2].set_title('Limited axes limits (lower data shown),\nsaturated cbar 0.1-0.99 quantile', fontsize=csize)    
+
+
+
+    # CONFUSION MATRIX-LIKE PLOT
+    n_squares=15
+    # Create a sample
+    sample_um=pd.DataFrame({x_name:transform_uniform_margins.transform_from_data_scale_to_uniform_margins_empirically(np.linspace(min_x,max_x,n_squares)),
+                         y_name:transform_uniform_margins.transform_from_data_scale_to_uniform_margins_empirically(np.linspace(min_y,max_y,n_squares))})
+    sample_ds=pd.DataFrame({x_name:np.linspace(min_x,max_x,n_squares),
+                                    y_name:np.linspace(min_y,max_y,n_squares)})
+
+    # Create sample grid
+    xv_um, yv_um = np.meshgrid(sample_um[x_name], sample_um[y_name])    #uniform margins
+    xv_ds, yv_ds = np.meshgrid(sample_ds[x_name], sample_ds[y_name])    #data scale
+    # mesh grid on uniform margins for calculating, in data scale
+    #   for plotting
+      
+    # Determine mid point of each pixel to calculate return
+    #   period for
+    mid_point_x_um=(xv_um[1:,1:]+xv_um[:-1,:-1])/2
+    mid_point_y_um=(yv_um[1:,1:]+yv_um[:-1,:-1])/2
+    mid_point_x_ds=(xv_ds[1:,1:]+xv_ds[:-1,:-1])/2
+    mid_point_y_ds=(yv_ds[1:,1:]+yv_ds[:-1,:-1])/2
+
+    # Reshape
+    raveled_mid_point_x=mid_point_x_um.ravel()
+    raveled_mid_point_y=mid_point_y_um.ravel()
+    raveled_mid_point_x_ds=mid_point_x_ds.ravel()
+    raveled_mid_point_y_ds=mid_point_y_ds.ravel()
+    
+    sample_grid=np.array([raveled_mid_point_x,raveled_mid_point_y]).T
+    
+    # Calculate return period
+    return_period=calculate_return_period(copula, sample_grid, block_size=block_size)
+    # Reshape for mesh grid
+    shaped_return_period=return_period.reshape(mid_point_x_um.shape)
+
+
+    # Initialise figure 
+    csize=20
+    fig_cm,ax_cm=plt.subplots(figsize=(20,20))
+
+    # Shape the data for the seaborn heatmap
+    df=pd.DataFrame({x_label:np.round(raveled_mid_point_x_ds, decimals=2),y_label:np.round(raveled_mid_point_y_ds, decimals=2), 'Return Period':return_period})
+    rect=df.pivot(index=y_label, columns=x_label, values='Return Period')
+    
+    # Plot the heatmap
+    ax_cm=sns.heatmap(rect, annot=True, cmap='plasma', fmt='.2g', square=True, ax=ax_cm, 
+                      norm=colors.LogNorm(vmin=np.quantile(shaped_return_period, 0.1), vmax=np.quantile(shaped_return_period, 0.99)), 
+                      cbar_kws={'label':'Return Period (years)', 'extend':'max'},
+                      annot_kws={'fontsize':csize})
+    ax_cm.invert_yaxis()
+    
+    # Some decor
+    ax_cm.tick_params(axis='both', labelsize=csize)
+    ax_cm.xaxis.get_label().set_fontsize(csize)
+    ax_cm.yaxis.get_label().set_fontsize(csize)
+    
+    cbar = ax_cm.collections[0].colorbar
+    cbar.ax.tick_params(labelsize=csize)
+    ax_cm.figure.axes[-1].yaxis.label.set_size(csize) #cbar label fontsize
+
+    #return return_period,raveled_mid_point_x,raveled_mid_point_y
+
+
+
+
+
+
+
+
+    
