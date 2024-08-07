d9b93b5ffcc00c4bf23b91fc309301801f697e1a
Alexandra Ruth Fogg <arfogg@cp.dias.ie>
2024-08-07 16:59:28 +0100

    code tidying (comments, docstrings, PEP8) for bootstrap_data.py, bootstrap_gevd_fit.py

--- src/bivariate/bootstrap_gevd_fit.py
+++ src/bivariate/bootstrap_gevd_fit.py
@@ -4,16 +4,18 @@ Created on Fri Aug  2 12:09:18 2024
 
 @author: A R Fogg
 
-Bootstrap GEVD fit class definition
+Bootstrap GEVD fit class definition.
 """
 
 from . import return_period_plot_1d
 from .gevd_fitter import gevd_fitter
 
+
 class bootstrap_gevd_fit():
     """
     A class which contains bootstrapped data and their fits.
     """
+
     def __init__(self, bootstrap_extrema, true_fit):
         """
         Initialise class
@@ -21,9 +23,36 @@ class bootstrap_gevd_fit():
         Parameters
         ----------
         bootstrap_extrema : np.array
-            Bootstrapped extrema. Of shape n_extrema x 
+            Bootstrapped extrema. Of shape n_extrema x
             n_bootstraps.
-
+        true_fit : gevd_fitter class
+            Object containing GEVD fitting information for the true data.
+            Contains attributes listed below. See gevd_fitter.py for
+            definition.
+            extremes : np.array
+                List of extrema the model is fit to.
+            extremes_unif_empirical : np.array
+                Parsed extrema converted to uniform margins empirically.
+            extremes_unif_CDF : np.array
+                Parsed extrema converted to uniform margins using the fitted
+                GEVD or Gumbel distribution.
+            distribution_name : string
+                String name of fitted distribution. Either 'genextreme' or
+                'gumbel_r'.
+            distribution : scipy.rv_continuous
+                Empty / generic distribution of selected distribution.
+            frozen_dist : frozen scipy.rv_continuous
+                Frozen version of fitted distribution.
+            shape_ : float
+                Fitted shape parameter.
+            location : float
+                Fitted location parameter.
+            scale : float
+                Fitted scale parameter.
+            formatted_dist_name : string
+                A formatted version of distribution name for plot labels.
+            aic : float
+                Akaike Information Criterion for fit.
         Returns
         -------
         None.
@@ -31,25 +60,43 @@ class bootstrap_gevd_fit():
         """
         # Store the bootstrapped data
         self.bs_data = bootstrap_extrema
-        
+
         # Store the number of extrema and bootstraps
         self.n_extrema = bootstrap_extrema.shape[0]
         self.n_ci_iterations = bootstrap_extrema.shape[1]
-        
+
         # Store the fit to the true data
         self.true_fit = true_fit
-        
+
+        # Fit distributions to each bootstrap
         self.fit_gevd()
-        
+
     def fit_gevd(self):
-        
+        """
+        Fit a GEVD model to each individual bootstrapped
+        dataset, and store an array of gevd_fitter classes.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        # Initialise empty list
         gevd_fitter_arr = []
+
+        # Looping through individual bootstraps
         for i in range(self.n_ci_iterations):
-            gevd_fitter_arr.append( gevd_fitter(self.bs_data[:,i], dist = self.true_fit.distribution_name) )
-        
+            # Create and append a gevd_fitter for each bootstrap
+            gevd_fitter_arr.append(gevd_fitter(
+                self.bs_data[:, i],
+                dist=self.true_fit.distribution_name))
+
+        # Assign the list of gevd_fitters to this class
         self.gevd_fitter_arr = gevd_fitter_arr
-        
-    def calc_return_levels(self, distribution_name, block_size, bs_return_periods, true_fit):
+
+    def calc_return_levels(self, distribution_name, block_size,
+                           bs_return_periods, true_fit):
         """
         Calculate the return levels for these bootstrapped extrema.
 
@@ -70,20 +117,17 @@ class bootstrap_gevd_fit():
         None.
 
         """
-        
+
         # Store the parsed information
         self.distribution_name = distribution_name
         self.block_size = block_size
         self.periods = bs_return_periods
-        
+
         # Calculate the return levels
-        levels, shape_, location, scale = return_period_plot_1d.return_level_bootstrapped_data(self, 
-                                                                                               self.n_ci_iterations, 
-                                                                                               bs_return_periods)
-        
-        # levels, shape_, location, scale = return_period_plot_1d.return_level_bootstrapped_data(self.bs_data, 
-        #                                 self.n_ci_iterations, self.distribution_name,
-        #                                 self.block_size, bs_return_periods, true_fit)
+        levels, shape_, location, scale = \
+            return_period_plot_1d.\
+            return_level_bootstrapped_data(self, self.n_ci_iterations,
+                                           bs_return_periods)
 
         # Store the return levels and associated fit information
         self.levels = levels
