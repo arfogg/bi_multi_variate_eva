b5714e7f81b63e1ddbb9b55f0aa9557e45a0905c
Alexandra Ruth Fogg <arfogg@cp.dias.ie>
2024-08-12 14:50:50 +0100

    tidying fit_copula_to_extremes.py

--- src/bivariate/fit_copula_to_extremes.py
+++ src/bivariate/fit_copula_to_extremes.py
@@ -3,12 +3,15 @@
 Created on Wed Jul 19 13:40:02 2023
 
 @author: A R Fogg
+
+Functions to fit copulae to data.
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from copulas.bivariate import Bivariate
+
 
 def fit_copula_bivariate(x_extremes, y_extremes, x_name, y_name):
     """
@@ -31,7 +34,7 @@ def fit_copula_bivariate(x_extremes, y_extremes, x_name, y_name):
         fitted copula
 
     """
-    
+
     # First, check same number of extremes in x and y
     if np.array(x_extremes).size == np.array(y_extremes).size:
         print('Fitting copula to parsed extremes')
@@ -39,21 +42,23 @@ def fit_copula_bivariate(x_extremes, y_extremes, x_name, y_name):
         print('ERROR: fit_copula_to_extremes.fit_copula_bivariate')
         print('x_extremes and y_extremes must have the same length')
         raise NameError('x_extremes and y_extremes must have the same length')
-       
+
     # Format the extremes to how copulas wants them
-    copula_arr=np.array([x_extremes,y_extremes]).T
-    
-    # Initialise the copula - testing with gumbel 
+    copula_arr = np.array([x_extremes, y_extremes]).T
+
+    # Initialise the copula - using Gumbel
     #   (options are clayton, frank, gumbel or independence)
-    copula=Bivariate(copula_type='gumbel')
-    
+    copula = Bivariate(copula_type='gumbel')
+
     # Fit the copula to the extremes
     copula.fit(copula_arr)
-    
+
     return copula
 
-def qualitative_copula_fit_check_bivariate(x_extremes, y_extremes, x_sample,
-                                    y_sample, x_name, y_name):
+
+def qualitative_copula_fit_check_bivariate(x_extremes, y_extremes,
+                                           x_sample, y_sample,
+                                           x_name, y_name):
     """
     Function to do a qualitative diagnostic plot for copula fit.
 
@@ -80,67 +85,71 @@ def qualitative_copula_fit_check_bivariate(x_extremes, y_extremes, x_sample,
         Axes from fig.
 
     """
-   
-    csize=15
-    fig, ax=plt.subplots(ncols=3, figsize=(27,7))
-    
+
+    fontsize = 15
+    fig, ax = plt.subplots(ncols=3, figsize=(27, 7))
+
     # OBSERVED EXTREMES
-    h_data=ax[0].hist2d(x_extremes, y_extremes, bins=[10,10], cmap='magma', cmin=1)
-    
+    h_data = ax[0].hist2d(x_extremes, y_extremes, bins=[10, 10],
+                          cmap='magma', cmin=1)
+
     # Some decor
-    ax[0].set_xlabel(x_name, fontsize=csize)
-    ax[0].set_ylabel(y_name, fontsize=csize)  
+    ax[0].set_xlabel(x_name, fontsize=fontsize)
+    ax[0].set_ylabel(y_name, fontsize=fontsize)
     for label in (ax[0].get_xticklabels() + ax[0].get_yticklabels()):
-        label.set_fontsize(csize)
+        label.set_fontsize(fontsize)
     ax[0].set_facecolor('darkgray')
-    t_data=ax[0].text(0.06, 0.94, '(a)', transform=ax[0].transAxes, fontsize=csize,  va='top', ha='left')
+    t_data = ax[0].text(0.06, 0.94, '(a)', transform=ax[0].transAxes,
+                        fontsize=fontsize,  va='top', ha='left')
     t_data.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='grey'))
-    ax[0].set_title('Observed Extremes', fontsize=csize)
-    
+    ax[0].set_title('Observed Extremes', fontsize=fontsize)
+
     # Colourbar
-    cbar_data=fig.colorbar(h_data[3], ax=ax[0])
-    cbar_data.set_label('Occurrence', fontsize=csize)
-    cbar_data.ax.tick_params(labelsize=csize)
+    cbar_data = fig.colorbar(h_data[3], ax=ax[0])
+    cbar_data.set_label('Occurrence', fontsize=fontsize)
+    cbar_data.ax.tick_params(labelsize=fontsize)
 
     # COPULA SAMPLE
-    h_sample=ax[1].hist2d(x_sample, y_sample, bins=[10,10], cmap='magma', cmin=1)
-    
+    h_sample = ax[1].hist2d(x_sample, y_sample, bins=[10, 10], cmap='magma',
+                            cmin=1)
+
     # Some decor
-    ax[1].set_xlabel(x_name, fontsize=csize)
-    ax[1].set_ylabel(y_name, fontsize=csize)  
+    ax[1].set_xlabel(x_name, fontsize=fontsize)
+    ax[1].set_ylabel(y_name, fontsize=fontsize)
     for label in (ax[1].get_xticklabels() + ax[1].get_yticklabels()):
-        label.set_fontsize(csize)
+        label.set_fontsize(fontsize)
     ax[1].set_facecolor('darkgray')
-    t_data=ax[1].text(0.06, 0.94, '(b)', transform=ax[1].transAxes, fontsize=csize,  va='top', ha='left')
+    t_data = ax[1].text(0.06, 0.94, '(b)', transform=ax[1].transAxes,
+                        fontsize=fontsize,  va='top', ha='left')
     t_data.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='grey'))
-    ax[1].set_title('Copula Sample (data scale)', fontsize=csize)
-    
+    ax[1].set_title('Copula Sample (data scale)', fontsize=fontsize)
 
     # Colourbar
-    cbar_sample=fig.colorbar(h_sample[3], ax=ax[1])
-    cbar_sample.set_label('Occurrence', fontsize=csize)
-    cbar_sample.ax.tick_params(labelsize=csize)
-    
+    cbar_sample = fig.colorbar(h_sample[3], ax=ax[1])
+    cbar_sample.set_label('Occurrence', fontsize=fontsize)
+    cbar_sample.ax.tick_params(labelsize=fontsize)
+
     # Both both sets of component-wise maxima as a scatter plot
-    ax[2].plot(x_extremes, y_extremes, marker='^', color="indigo", label='Observations',
-                  fillstyle='none', linewidth=0.)
-    ax[2].plot(x_sample, y_sample, marker='*', color="darkgoldenrod", label="Copula Sample",
-                  fillstyle='none', linewidth=0.)
+    ax[2].plot(x_extremes, y_extremes, marker='^', color="indigo",
+               label='Observations', fillstyle='none', linewidth=0.)
+    ax[2].plot(x_sample, y_sample, marker='*', color="darkgoldenrod",
+               label="Copula Sample", fillstyle='none', linewidth=0.)
+
     # Some decor
-    ax[2].set_xlabel(x_name, fontsize=csize)
-    ax[2].set_ylabel(y_name, fontsize=csize)  
+    ax[2].set_xlabel(x_name, fontsize=fontsize)
+    ax[2].set_ylabel(y_name, fontsize=fontsize)
     for label in (ax[2].get_xticklabels() + ax[2].get_yticklabels()):
-        label.set_fontsize(csize)
-    t_data=ax[2].text(0.06, 0.94, '(c)', transform=ax[2].transAxes, fontsize=csize,  va='top', ha='left')
+        label.set_fontsize(fontsize)
+    t_data = ax[2].text(0.06, 0.94, '(c)', transform=ax[2].transAxes,
+                        fontsize=fontsize,  va='top', ha='left')
     t_data.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='grey'))
-    ax[2].set_title('Comparison', fontsize=csize)
-    ax[2].legend(fontsize=csize)
-    
+    ax[2].set_title('Comparison', fontsize=fontsize)
+    ax[2].legend(fontsize=fontsize)
+
     # Make axes limits sample as for panel0 for panels 1 and 2
     ax[1].set_xlim(ax[0].get_xlim())
     ax[1].set_ylim(ax[0].get_ylim())
     ax[2].set_xlim(ax[0].get_xlim())
-    ax[2].set_ylim(ax[0].get_ylim())    
-    
+    ax[2].set_ylim(ax[0].get_ylim())
+
     return fig, ax
-    
